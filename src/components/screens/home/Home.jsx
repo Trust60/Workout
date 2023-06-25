bdaddfd8eb4806f2958e80b2094430eb6b08334d
Trust60 <dmerteyhrabovetski@gmail.com>
2023-06-25 22:30:12 +0300

    Create home page

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,9 +1,20 @@
+import { useNavigate } from 'react-router-dom'
+
+import { useAuth } from '../../../hooks/useAuth'
 import Layout from '../../layout/Layout'
+import Button from '../../ui/button/Button'
+
+import styles from './Home.module.scss'
 
 function Home() {
+	const { isAuth } = useAuth()
+	const navigate = useNavigate()
 	return (
-		<Layout>
-			<h1>Home</h1>
+		<Layout bgImage={'/images/home-bg.jpg'}>
+			<Button clickHandler={() => navigate(isAuth ? '/new-workout' : '/auth')}>
+				{isAuth ? 'New' : 'Sign in'}
+			</Button>
+			<h1 className={styles.heading}>EXERCISES FRO THE SHOULDERS</h1>
 		</Layout>
 	)
 }
